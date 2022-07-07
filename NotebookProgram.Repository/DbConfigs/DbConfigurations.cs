3e989c3817afeed486bc348e5f5da4c1861ab8c4
DaliusGrigolius <dalius.grigolius@gmail.com>
2022-07-08 00:23:00 +0300

    add dbcontext

---
+++ NotebookProgram.Repository/DbConfigs/DbConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotebookProgram.Repository.DbConfigs
+{
+    public class DbConfigurations : IDbConfigurations
+    {
+        public string ConnectionString { get; set; }
+        public DbContextOptions Options { get; set; }
+
+        public DbConfigurations(IConfiguration config)
+        {
+            ConnectionString = config.GetConnectionString("myDb1");
+            Options = new DbContextOptionsBuilder().UseSqlServer(ConnectionString).Options;
+        }
+    }
+}
---
+++ NotebookProgram.Repository/DbConfigs/IDbConfigurations.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NotebookProgram.Repository.DbConfigs
+{
+    public interface IDbConfigurations
+    {
+        string ConnectionString { get; set; }
+        DbContextOptions Options { get; set; }
+    }
+}
